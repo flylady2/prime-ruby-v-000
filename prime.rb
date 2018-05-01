2512b21b1d4f79ce758e363fde23de37fe2a2828
Leslie Stevens <flylady@mail.utexas.edu>
2018-05-01 18:18:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
-# Add  code here!
+def prime?(number)
+
+  if number <= 1
+    return false
+  elsif number == 2
+    return true
+  end
+  
+  i = 0
+  array = *(2..number)
+  while i < array.length
+    array.each do |num|
+      if number % num == 0
+        return false
+      elsif number % != 0
+        i += 1
+      end
+    true
+    end
+  end
+end# Add  code here!
